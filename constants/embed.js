7fa70ad01c3b1fc5e89cd01b27498ec2539fb165
TuanPham <tuan.pham@team.enouvo.com>
2022-08-26 17:57:47 +0700

    change url iframe

--- constants/embed.js
+++ constants/embed.js
@@ -1,6 +1,6 @@
 const embedMovie = (id) =>
-    `https://www.2embed.org/embed/movie?tmdb=${id}`;
+    `https://www.2embed.to/embed/tmdb/movie?id=${id}`;
 const embedEpisode = (id, season, episode) =>
-    `https://www.2embed.org/embed/tv?tmdb=${id}&sea=${season}&epi=${episode}`;
+    `https://www.2embed.to/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`;
  
 export { embedMovie, embedEpisode };
